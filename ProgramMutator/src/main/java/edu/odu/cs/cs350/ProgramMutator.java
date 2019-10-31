a55d7b88dc6c115464c3b53cf227ba0a7359b8eb
bdemerch <bdemerch@cs.odu.edu>
2019-10-31 16:39:05 -0400

    Update ProgramMutator.java

--- ProgramMutator/src/main/java/edu/odu/cs/cs350/ProgramMutator.java
+++ ProgramMutator/src/main/java/edu/odu/cs/cs350/ProgramMutator.java
@@ -1,5 +1,9 @@
 package edu.odu.cs.cs350;
 
 public class ProgramMutator {
+      public static void main(String[] args) {
+	    System.out.println("Hello Mutant World");
+	    System.err.println("no errors");
+	  }
 
 }
