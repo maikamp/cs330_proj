379defd803fc63ba7828bc93cafe73a3fef39106
bdemerch <bdeme004@odu.edu>
2019-11-01 13:42:09 -0400

    very basic cli should be up

--- ProgramMutator/src/main/java/edu/odu/cs/cs350/ProgramMutator.java
+++ ProgramMutator/src/main/java/edu/odu/cs/cs350/ProgramMutator.java
@@ -1,11 +1,35 @@
 package edu.odu.cs.cs350;
 
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class ProgramMutator {
 	
-	  public static void main(String[] ar) {
-		    System.out.println("Hello Mutant World");
-		    System.err.println("no errors");
+	  public static void main(String[] args) {
+
+		    Options options = new Options();
+		    CommandLineParser parser = new DefaultParser();
+		    HelpFormatter formatter = new HelpFormatter();
+
+		    
+		    try {
+		      CommandLine cmd = parser.parse(options, args);
+		      String dirPath = cmd.getArgs()[0];
+		          
+		    } 
+		    catch (ArrayIndexOutOfBoundsException e) {
+		    	System.err.println(e);
+		    }
+		    catch (ParseException e) {
+		      System.err.println(e.getMessage());
+		    }
 		  }
-}
+		  }
+
 
 
