aac4aa0e09bdc2121b3cdfcaa415db05bff42f69
mcampbel <mcamp004@odu.edu>
2019-11-02 04:17:43 -0400

    added Mutation nested class in MutationGenerator, implementation Mutant

--- ProgramMutator/src/main/java/edu/odu/cs/cs350/MutationGenerator.java
+++ ProgramMutator/src/main/java/edu/odu/cs/cs350/MutationGenerator.java
@@ -3,9 +3,21 @@ package edu.odu.cs.cs350;
 class MutationGenerator {
 /**
  * getArthmeticOperator() -> Collection
-getArthmeticOperator() -> Collection
-getConstantOperator() -> Collection
-getVariableOperator() -> Collection
-generateMutants(MutationOperator mop, goldCode g)
+ * getArthmeticOperator() -> Collection
+ * getConstantOperator() -> Collection
+ * getVariableOperator() -> Collection
+ * generateMutants(MutationOperator mop, GoldCode g)
+ * 
+ * based on kennedy's "mutations: Collection" in the outline from our meeting, it 
+ * looks like we need an ADT for mutations in order to hold them in a collection. I've 
+ * added one in this class, but we can move it if it seems more appropriate elsewhere. -Mike
  */
+	class Mutation{
+		String mutOperator; //we could also use an int to identify the type of change made, e.g. the mutation -Mike
+		Mutation(String mutOperator)
+		{
+			this.mutOperator = "";
+		}
+		
+	}
 }
