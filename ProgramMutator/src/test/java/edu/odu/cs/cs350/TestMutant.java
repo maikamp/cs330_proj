1272e039ae6d4d7daf01da16130ff6541b8f3f21
mcampbel <mcamp004@odu.edu>
2019-11-02 19:20:35 -0400

    added test classes for mutant and mutationTestState for TDD

---
+++ ProgramMutator/src/test/java/edu/odu/cs/cs350/TestMutant.java
@@ -0,0 +1,34 @@
+package edu.odu.cs.cs350;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+public class TestMutant {
+	
+	@Test
+	public void testMutantConstructor()
+	{
+		
+	}
+	@Test
+	public void testSetID()
+	{
+		
+	}
+	@Test
+	public void testSetStatus()
+	{
+		
+	}
+	@Test
+	public void testSetViable()
+	{
+		
+	}
+	@Test
+	public void testMutationList()
+	{
+		
+	}
+}
