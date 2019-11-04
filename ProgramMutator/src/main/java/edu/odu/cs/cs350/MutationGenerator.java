237f00478bfab48209d96a58ed938f6c3f01f67d
joshu <joshu@Joshuas-PC>
2019-11-03 21:33:46 -0500

    basic loop to generate an array of mutant IDs

--- ProgramMutator/src/main/java/edu/odu/cs/cs350/MutationGenerator.java
+++ ProgramMutator/src/main/java/edu/odu/cs/cs350/MutationGenerator.java
@@ -8,4 +8,22 @@ getConstantOperator() -> Collection
 getVariableOperator() -> Collection
 generateMutants(MutationOperator mop, goldCode g)
  */
+	
+	int id;
+	int mutantArray[]; 
+	
+	
+	for (int i=0; i<100; i++) {
+		
+		int random = (int)(Math.random()*100000);
+		mutantArray[i]=  random;
+		System.out.print(random);
+		
+		
+		/// Basic array to generate mutant IDs ranging from 1-100000
+	}
+	
+	
+	
+	
 }
