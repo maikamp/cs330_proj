7817e8141c00576d6a582b8bd683e7b615fda326
nhughes <nhugh006@odu.edu>
2019-11-22 21:01:51 -0500

    Update Mutation Operator

--- ProgramMutator/src/main/java/edu/odu/cs/cs350/MutationOperator.java
+++ ProgramMutator/src/main/java/edu/odu/cs/cs350/MutationOperator.java
@@ -1,11 +1,64 @@
 package edu.odu.cs.cs350;
 
+import java.util.List;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.expr.EnclosedExpr;
+import com.github.javaparser.ast.expr.Expression;
+
 class MutationOperator {
-/**
- * ArithmeticOperators: Collection
-BooleanExpressionOperators: Collection
-ConstantOperators: Collection
-VariableOperators: Collection
-MutationOperator(Operator o)
- */
+	
+	private List<CompilationUnit> codeToModify;
+	private String AlterableMutant;
+	private String AlterMutant;
+	
+	//Receives Code and what needs to be changed.
+	//
+	MutationOperator(List<CompilationUnit> code, String MutantToChange){
+		codeToModify = code;
+		AlterableMutant = MutantToChange;
+	}
+	void getArithmeticOp() {
+		/**
+		 * Choose from list of arithmetic types
+		 * Set as AlterMutant
+		 */
+		String ArithmeticOps[] = {"+","-","*","/","%","+","-","*","/","%"};
+		int random = (int)(Math.random()*10);
+		String AlterMutant = ArithmeticOps[random];
+	}
+	void getBooleanOp() {
+		/**
+		 * Choose from list of boolean types
+		 * Set as AlterMutant
+		 */
+		String BooleanOps[] = {"|","&","^","!","||","&&","==","!="};
+		int random = (int)(Math.random()*8);
+		String AlterMutant = BooleanOps[random];
+		
+	}
+	void getConstOp() {
+		/**
+		 * This function will find a constant variable and change it's value
+		 */
+	}
+	void getVariableOp() {
+		/**
+		 * Choose from list of variable types
+		 * Set as AlterMutant
+		 */
+		String VarOps[] = {"int","String","char","boolean","byte","short","long","float","double"};
+		int random = (int)(Math.random()*8);
+		String AlterMutant = VarOps[random];
+	}
+	CompilationUnit getAlteredCode() {
+		CompilationUnit newCode = null;
+		/**
+		 * Get code from javaParser
+		 * Replace all instances of AlterableMutant with AlterMutant
+		 * Return updated code
+		 */
+	    return newCode;
+	}
 }
